65e8051a84843b9d72dd760d3e1e3362f46fbab1
kalwhyte <superkalel55@gmail.com>
2023-07-20 17:28:24 +0100

    backup

--- src/draw.c
+++ src/draw.c
@@ -30,7 +30,7 @@ void drawRect(int x, int y, int width, int height, color_t color)
 void drawLine(int x0, int y0, int x1, int y1, color_t color)
 {
 	float xIncrement, yIncrement, currentX, currentY;
-	int i, longestSideLength, deltaX,  deltaY;
+	int i, abs(), longestSideLength, deltaX,  deltaY;
 
 	deltaX = (x1 - x0);
 	deltaY = (y1 - y0);
